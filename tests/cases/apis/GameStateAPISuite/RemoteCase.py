562f2411cd45d1ed447352175d97da5f14292a6d
Luke Swanson <lwswanson2@wisc.edu>
2026-06-23 12:16:11 -0500

    Minimal work to get RemoteCase cases in line with using config class.

--- tests/cases/apis/GameStateAPISuite/RemoteCase.py
+++ tests/cases/apis/GameStateAPISuite/RemoteCase.py
@@ -1,61 +1,60 @@
 # import libraries
+import logging
 import requests
 from unittest import TestCase
+# import ogd libraries
+from ogd.apis.models.APIRequest import APIRequest
+from ogd.apis.models.APIResponse import APIResponse
+from ogd.common.utils.Logger import Logger
 # import locals
-from tests.t_config import settings
+from tests.PlayerAPITestConfig import PlayerAPITestConfig
+from tests.config import t_config
 
 class t_GameStateAPI(TestCase):
-    def RunAll(self):
-        t = t_GameStateAPI.t_GameState()
-        t.test_home()
-        t.test_get()
-        t.test_get_multi()
-        t.test_post()
+    @classmethod
+    def setUpClass(cls):
+        cls.testing_config = PlayerAPITestConfig.FromDict(name="PlayerAPITestConfig", unparsed_elements=t_config.settings)
 
-    class t_GameState:
-        def __init__(self):
-            self.TEST_PLAYER_ID = "ImmortanJoe"
-            self.TEST_GAME      = "AQUALAB"
+        _level = logging.DEBUG if cls.testing_config.Verbose else logging.INFO
+        Logger.std_logger.setLevel(_level)
+        cls.TEST_PLAYER_ID = "ImmortanJoe"
+        cls.TEST_GAME      = "AQUALAB"
 
-        def test_home(self):
-            base = settings['EXTERN_SERVER']
-            print(f"GET test at {base}")
-            result = requests.get(url=base)
-            if result is not None:
-                print(f"Result of get:\n{result.text}")
-            else:
-                print(f"No response to GET request.")
-            print()
+    def test_home(self):
+        print(f"GET test at {self.testing_config.ExternEndpoint}")
+        result = requests.get(url=base)
+        if result is not None:
+            print(f"Result of get:\n{result.text}")
+        else:
+            print(f"No response to GET request.")
+        print()
 
-        def test_get(self):
-            base = settings['EXTERN_SERVER']
-            url = f"{base}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
-            print(f"GET test at {url}")
-            params = { 'count':1, 'offset':0 }
-            result = requests.get(url=url, params=params)
-            if result is not None:
-                print(f"Result of get:\n{result.text}")
-            else:
-                print(f"No response to GET request.")
+    def test_get(self):
+        url = f"{self.testing_config.ExternEndpoint}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
+        print(f"GET test at {url}")
+        params = { 'count':1, 'offset':0 }
+        result = requests.get(url=url, params=params)
+        if result is not None:
+            print(f"Result of get:\n{result.text}")
+        else:
+            print(f"No response to GET request.")
 
-        def test_get_multi(self):
-            base = settings['EXTERN_SERVER']
-            url = f"{base}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
-            print(f"GET test at {url}")
-            params = { 'count':3, 'offset':0 }
-            result = requests.get(url=url, params=params)
-            if result is not None:
-                print(f"Result of get:\n{result.text}")
-            else:
-                print(f"No response to GET request.")
+    def test_get_multi(self):
+        url = f"{self.testing_config.ExternEndpoint}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
+        print(f"GET test at {url}")
+        params = { 'count':3, 'offset':0 }
+        result = requests.get(url=url, params=params)
+        if result is not None:
+            print(f"Result of get:\n{result.text}")
+        else:
+            print(f"No response to GET request.")
 
-        def test_post(self):
-            base = settings['EXTERN_SERVER']
-            url = f"{base}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
-            print(f"POST test at {url}")
-            params = { 'state':"{'data':'test data'}" }
-            result = requests.post(url=url, params=params)
-            if result is not None:
-                print(f"Result of post:\n{result.text}")
-            else:
-                print(f"No response to POST request.")
+    def test_post(self):
+        url = f"{self.testing_config.ExternEndpoint}/player/{self.TEST_PLAYER_ID}/game/{self.TEST_GAME}/state"
+        print(f"POST test at {url}")
+        params = { 'state':"{'data':'test data'}" }
+        result = requests.post(url=url, params=params)
+        if result is not None:
+            print(f"Result of post:\n{result.text}")
+        else:
+            print(f"No response to POST request.")
--- tests/cases/apis/PlayerIDAPISuite/RemoteCase.py
+++ tests/cases/apis/PlayerIDAPISuite/RemoteCase.py
@@ -1,50 +1,51 @@
 # import libraries
+import logging
 import requests
 from unittest import TestCase
+# import ogd libraries
+from ogd.apis.models.APIRequest import APIRequest
+from ogd.apis.models.APIResponse import APIResponse
+from ogd.common.utils.Logger import Logger
 # import locals
-from tests.t_config import settings
+from tests.PlayerAPITestConfig import PlayerAPITestConfig
+from tests.config import t_config
 
 class t_PlayerIDAPI(TestCase):
     """Testbed class for the PlayerID API.
     """
-    def RunAll(self):
-        t = t_PlayerIDAPI.t_Player()
-        t.test_home()
-        t.test_put()
-        t.test_get()
+    @classmethod
+    def setUpClass(cls):
+        cls.testing_config = PlayerAPITestConfig.FromDict(name="PlayerAPITestConfig", unparsed_elements=t_config.settings)
 
-    class t_Player:
-        def __init__(self):
-            self.TEST_PLAYER_ID = "ImmortanJoe"
-            self.TEST_GAME      = "AQUALAB"
+        _level = logging.DEBUG if cls.testing_config.Verbose else logging.INFO
+        Logger.std_logger.setLevel(_level)
 
-        def test_home(self):
-            base = settings['EXTERN_SERVER']
-            print(f"GET test at {base}")
-            result = requests.get(url=base)
-            if result is not None:
-                print(f"Result of get:\n{result.text}")
-            else:
-                print(f"No response to GET request.")
-            print()
+        cls.TEST_PLAYER_ID = "ImmortanJoe"
+        cls.TEST_GAME      = "AQUALAB"
 
-        def test_get(self):
-            base = settings['EXTERN_SERVER']
-            url = f"{base}/player/"
-            print(f"GET test at {url}")
-            result = requests.get(url=url)
-            if result is not None:
-                print(f"Result of get:\n{result.text}")
-            else:
-                print(f"No response to GET request.")
+    def test_home(self):
+        result = requests.get(url=self.testing_config.ExternEndpoint)
+        if result is not None:
+            print(f"Result of get:\n{result.text}")
+        else:
+            print(f"No response to GET request.")
+        print()
 
-        def test_put(self):
-            base = settings['EXTERN_SERVER']
-            url = f"{base}/player/"
-            print(f"POST test at {url}")
-            params = { "player_id":"test_player", "name":"Test" }
-            result = requests.put(url=url, params=params)
-            if result is not None:
-                print(f"Result of post:\n{result.text}")
-            else:
-                print(f"No response to POST request.")
+    def test_get(self):
+        url = f"{self.testing_config.ExternEndpoint}/player/"
+        print(f"GET test at {url}")
+        result = requests.get(url=url)
+        if result is not None:
+            print(f"Result of get:\n{result.text}")
+        else:
+            print(f"No response to GET request.")
+
+    def test_put(self):
+        url = f"{self.testing_config.ExternEndpoint}/player/"
+        print(f"POST test at {url}")
+        params = { "player_id":"test_player", "name":"Test" }
+        result = requests.put(url=url, params=params)
+        if result is not None:
+            print(f"Result of post:\n{result.text}")
+        else:
+            print(f"No response to POST request.")
